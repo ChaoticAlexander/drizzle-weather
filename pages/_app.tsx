cef2accdf73178d71920213a05020ab2ce81097f
ChaoticAlexander <tananikas@hotmail.com>
2024-02-22 18:14:27 +0100

    added title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import DefaultLayout from '@/app/layouts/root'
 import "@/app/styles/globals.css"
 import type { AppProps } from 'next/app'
@@ -6,9 +7,16 @@ import wrapper from '@/lib/store/store'
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
-    <DefaultLayout>
-      <Component {...pageProps} />
-    </DefaultLayout>
+    <>
+      <Head>
+        <title>Drizzle Weather</title>
+        <meta name="description" content="Drizzle Weather app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <DefaultLayout>
+        <Component {...pageProps} />
+      </DefaultLayout>
+    </>
   )
 }
 
